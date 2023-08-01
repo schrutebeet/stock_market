e96f7ddf65be7e27bb2497722facdb73b3edc9ed
schrute_beet <ricardosans98@gmail.com>
2023-08-01 23:07:22 +0200

    Start git init

---
+++ src/main.py
@@ -0,0 +1,47 @@
+import time
+import stock
+from stock import Stock
+from models import Model
+import sys
+from pathlib import Path
+api_dir = str(Path(stock.__file__).parent.parent.parent / "keys")
+sys.path.append(api_dir)
+from auth import api_key
+
+def timeit(func):
+    @staticmethod
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        # runs the function
+        function = func(*args, **kwargs)
+        end = time.time()
+        print("-"*25)
+        print(f'Elapsed time: {(end - start):.2f} seconds')
+        print("-"*25, "\n")
+        return function
+    return wrapper
+
+class Runner():
+    @timeit
+    def run(stock, fetch_type='fetch_daily', train_size=0.8, rolling_window=60, scale=True):
+        print(f'\n-------Running framework for {stock.stock_symbol}-------\n')
+        getattr(stock, fetch_type)()
+        stock.prepare_train_test_sets(train_size, rolling_window, scale=scale)
+        base_for_model = Model(stock)
+        if not base_for_model.stock_inst.data.empty:
+            base_for_model.lstm_nn(viz=True)
+
+
+def main():
+    arguments = sys.argv
+    try:
+        for arg in  arguments[1:]:
+            stock = Stock(arg)
+            Runner.run(stock, scale=True)
+    except Exception as err:
+        comment = "Please, add a stock symbol argument when running the framework."
+        err_message = f"{type(err).__name__}: {str(err)}. {comment}"
+        raise Exception(f"{err_message}")
+
+if __name__ == '__main__':
+    main()
