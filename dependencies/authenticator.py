b21e65732183cc2bac3e54ef4bbdfe701d28f392
schrute_beet <ricardosans98@gmail.com>
2023-08-05 02:46:25 +0200

    Create authentication script

---
+++ dependencies/authenticator.py
@@ -0,0 +1,8 @@
+import sys
+import src
+from pathlib import Path
+api_dir = str(Path(src.__file__).parent.parent.parent / "keys")
+sys.path.append(api_dir)
+from auth import api_key
+
+api_key = api_key
