1b2da529de4f60554260ad394f24054dfbe1fc86
Ricardo Sans <ricardosans98@gmail.com>
2023-11-05 14:37:23 +0100

    Add accuracy in main

--- src/main.py
+++ src/main.py
@@ -26,24 +26,23 @@ def timeit(func):
 class Runner:
     @timeit
     def run(fetch_type="fetch_daily", train_size=0.8, rolling_window=60, scale=True):
-        try:
-            log_config.add_separator()
-            logging.info(f"Initializing web scraper")
-            ind_scraper = IndustriesScraper("https://stockanalysis.com/stocks/")
-            scraped_tb = ind_scraper.run_scraper()
-        except:
-            return 1
-        for symbol in scraped_tb.symbol[:]:
+        log_config.add_separator()
+        logging.info(f"Initializing web scraper")
+        ind_scraper = IndustriesScraper("https://stockanalysis.com/stocks/")
+        scraped_tb = ind_scraper.run_scraper()
+        for symbol in scraped_tb.symbol[:2]:
             try:
                 stock = Stock(symbol)
                 getattr(stock, fetch_type)()
                 stock.prepare_train_test_sets(train_size, rolling_window, scale=scale)
                 base_for_model = Model(stock)
-                base_for_model.lstm_nn(viz=False)
-                logging.info(f"Successfully run framework for symbol {symbol}")
+                accuracy = base_for_model.lstm_nn(viz=False)
+                logging.info(f"Successfully run framework for symbol {symbol}. Score: {accuracy*100:.2f}")
+                log_config.add_separator()
             except Exception as e:
                 logging.error(f"Process aborted for symbol {symbol}")
-                pass
+                log_config.add_separator()
+        logging.info("SUCCESS: END OF PROCESS")
 
 
 def main():
