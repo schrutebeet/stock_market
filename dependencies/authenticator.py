43e83845c1f63120a61405f79a0f0e98b518bc53
Ricardo Sans <ricardosans98@gmail.com>
2023-11-05 16:07:02 +0100

    Add postgress credentials logger

--- dependencies/authenticator.py
+++ dependencies/authenticator.py
@@ -1,8 +1,22 @@
 import sys
-import src
+import yaml
 from pathlib import Path
-api_dir = str(Path(src.__file__).parent.parent.parent / "keys")
-sys.path.append(api_dir)
-from alpha_key import api_key
 
+key_dir = str(Path(__file__).parent.parent.parent / "keys")
+sys.path.append(key_dir)
+
+from alpha_key import api_key
 api_key = api_key
+
+with open(key_dir / "postgres_keys.yaml", 'r') as f:
+            credentials = yaml.safe_load(f)
+
+class Settings:
+    PROJECT_NAME: str = "My first connection"
+    PROJECT_VERSION: str = "0.0.1"
+
+    POSTGRES_USER: str = credentials["user"]
+    POSTGRES_PASSWORD = credentials["password"]
+    POSTGRES_HOST: str = credentials["host"]
+    POSTGRES_PORT: str = credentials["port"]
+    POSTGRES_DATABASE: str = credentials["database"]
