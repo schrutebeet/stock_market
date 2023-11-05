b135ed9159253c836ba86e8c0390818bafb5e050
Ricardo Sans <ricardosans98@gmail.com>
2023-11-05 14:24:41 +0100

    Avoid keyspath printing

--- dependencies/authenticator.py
+++ dependencies/authenticator.py
@@ -2,7 +2,6 @@ import sys
 import src
 from pathlib import Path
 api_dir = str(Path(src.__file__).parent.parent.parent / "keys")
-print(api_dir)
 sys.path.append(api_dir)
 from alpha_key import api_key
 
