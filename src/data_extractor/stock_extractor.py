b37e70c7c695e77ac7d12639a92b25585aa069e5
Ricardo Sans <ricardosans98@gmail.com>
2023-11-08 02:18:56 +0100

    Fix bugs fro stock Exctractor

--- src/data_extractor/stock_extractor.py
+++ src/data_extractor/stock_extractor.py
@@ -15,8 +15,8 @@ class StocksExtractor(BaseExtractor):
 
     ACCEPTABLE_PERIODS = ["1min", "5min", "15min", "30min", "60min", "daily"]
 
-    def __init__(self, symbol: str, api_key: Any, function: str):
-        super().__init__(symbol, api_key, function)
+    def __init__(self, symbol: str, api_key: Any):
+        super().__init__(symbol, api_key)
 
     def get_data(self, 
                  period: str = "daily", 
@@ -46,20 +46,20 @@ class StocksExtractor(BaseExtractor):
                                             {', '.join(self.ACCEPTABLE_PERIODS)}"
             )
 
-        json_rates = {}
+        json_list = []
         current_date = datetime.now()
-        current_month = from_date
-        while current_month <= until_date:
-            month_datetime = datetime.strptime(current_month, "%Y-%m-%d")
-            month_str = month_datetime.strftime("%Y-%m")
+        current_month = datetime.strptime(from_date, "%Y-%m-%d")
+        while current_month <= datetime.strptime(until_date, "%Y-%m-%d"):
+            month_str = current_month.strftime("%Y-%m")
+            print(month_str)
             new_data = self.__choose_function_type(period, month_str)
-            json_rates.update(new_data)
+            json_list.append(new_data)
             logging.info(
                 f"Extracting stock information for {month_str}"
             )
             current_month += relativedelta(months=1)
-
-        df = pd.DataFrame(json_rates).T
+        joining_dicts = {key: value for d in json_list for key, value in d.items()}
+        df = pd.DataFrame(joining_dicts).T
         if period != "daily":
             df.columns = ["open", "high", "low", "close", "volume"]
         else:
@@ -70,6 +70,7 @@ class StocksExtractor(BaseExtractor):
         # Apply specific daydate filters
         start_date = pd.to_datetime(f"{from_date} 00:00:00")
         end_date = pd.to_datetime(f"{until_date} 23:59:59")
+        print(df)
         df = df[(df.index >= start_date) & (df.index <= end_date)]
 
         return df
@@ -88,17 +89,16 @@ class StocksExtractor(BaseExtractor):
         if period != "daily":
             url = (
                 f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="
-                f"{self.symbol}&month={month}&interval={period}&apikey={self.api_key}"
+                f"{self.symbol}&outputsize=full&month={month}&interval={period}&apikey={self.api_key}"
             )
             r = requests.get(url)
-            json_data = r.json()[f"Time Series {period}"]
+            json_data = r.json()[f"Time Series ({period})"]
         else:
             url = (
                 f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol="
-                f"{self.symbol}&interval={period}&apikey={self.api_key}"
+                f"{self.symbol}&outputsize=full&apikey={self.api_key}"
             )
             r = requests.get(url)
             json_data = r.json()["Time Series (Daily)"]
 
-            return json_data
-
+        return json_data
