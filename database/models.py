eb89ccc0edcb35289bf0038113f855e76f4e5793
Ricardo Sans <ricardosans98@gmail.com>
2023-11-19 14:38:16 +0100

    Create function to create any type of model

--- database/models.py
+++ database/models.py
@@ -1,6 +1,6 @@
 from database.connection import Base
 from sqlalchemy.dialects.postgresql import TIMESTAMP
-from sqlalchemy import String, Column, Text, DateTime
+from sqlalchemy import String, Column, Text, DateTime, Float
 from datetime import datetime
 
 # this Item model (table) stems from the Base class and has its properties 
@@ -40,3 +40,18 @@ class StockMerge(Base):
     ipodate = Column(DateTime)
     industry = Column(Text)
     marketcap = Column(String)
+
+def create_dynamic_model(class_name, schema_name, column_data):
+    # Define the attributes for the class
+    class_attributes = {
+        "__tablename__": class_name.lower(),
+        "__table_args__": {'schema': schema_name}
+    }
+
+    # Add columns to the class attributes
+    for column_name, column_type in column_data.items():
+        class_attributes[column_name] = Column(**column_type)
+
+    # Create the class using the type function
+    dynamic_class = type(class_name, (Base,), class_attributes)
+    return dynamic_class
