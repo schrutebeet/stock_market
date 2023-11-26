a946e8ddca81fcc16054b74bb1363e582eea30b6
Ricardo Sans <ricardosans98@gmail.com>
2023-11-26 22:08:23 +0100

    Add get_chrome_driver_path method

--- config/config.py
+++ config/config.py
@@ -1,3 +1,20 @@
+from pathlib import Path
+from utils.error_handling import DriverError
+from utils.detect_os import detect_os
 
 class Config:
-    chromedriver_path = r"C:\Users\Ricky\Documents\Gitlab\chrome-win64\chrome.exe"
+    def __init__(self) -> None:
+        self.os = detect_os()
+
+    def get_chrome_driver_path(self) -> Path:
+
+        project_dir = Path(__file__).parent.parent
+
+        if self.os == "windows":
+            chromedriver_path = project_dir / Path("drivers/chrome-win64/chrome.exe")
+        elif self.os == "linux":
+            chromedriver_path = project_dir / Path("drivers/chromium-browser/chromedriver")
+        else:
+            raise DriverError
+
+        return chromedriver_path
