0f342a93712949e0d9ecdd94ee423a5a2033a376
Ricardo Sans <ricardosans98@gmail.com>
2023-12-31 20:16:20 +0100

    Rearrange main

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ from src.stock import Stock
 from utils.util_funcs import timeit
 from database.utils_db import UtilsDB
 from utils.dafault_columns import default_daily, default_minutes
+from email_notifications.email_generator import EmailGenerator
 
 def main():
     log_config.add_separator()
@@ -20,27 +21,23 @@ def main():
     scraped_tb = ind_scraper.run_scraper()
     stock_dictionary = {}
     utils_db = UtilsDB()
-    for symbol in scraped_tb.symbol[:10]:
+    for symbol in scraped_tb.symbol[:]:
         # initiation the stock class for particular stock
         stock_dictionary[f"{symbol}"] = Stock(symbol)
         # Fetching OHLCV data on that stock
         stock_extractor = stock_dictionary[f"{symbol}"].extractor
         stock_dictionary[f"{symbol}_daily"] = stock_extractor.get_data(period="daily")
         stock_dictionary[f"{symbol}_1min"] = stock_extractor.get_data(period="1min")
-    
-    for symbol in scraped_tb.symbol[:10]:
+
+    for symbol in scraped_tb.symbol[:]:
         model_daily = utils_db.create_specific_model(class_name=f"{symbol}_daily", model_name=f"{symbol}_daily", 
                                                         schema_name="daily_quotes", column_data=copy.deepcopy(default_daily))
-        model_minute = utils_db.create_specific_model(class_name=symbol+"1min", model_name=symbol.lower()+"_1min", 
+        model_minute = utils_db.create_specific_model(class_name=f"{symbol}_1min", model_name=f"{symbol}_1min", 
                                                         schema_name="onemin_quotes", column_data=copy.deepcopy(default_minutes))
         # Store daily data in DB
         utils_db.insert_df_in_db(stock_dictionary[f"{symbol}_daily"], model_daily)
         utils_db.insert_df_in_db(stock_dictionary[f"{symbol}_1min"], model_minute)
 
-    # Commit the changes to the database for each model
-    utils_db.dbsession.commit()
-    # Close connection
-    utils_db.dbsession.close()
             # Saving data in DB
             # utils_db = UtilsDB()
             # # Create daily and minute tables for particular stock (if they do not exist)
@@ -63,6 +60,9 @@ def main():
             # accuracy = base_for_model.lstm_nn(viz=False)
             # logger.info(f"Successfully run framework for symbol {symbol}. Score: {accuracy*100:.2f}%")
     logger.info("SUCCESS: END OF PROCESS")
+    email = EmailGenerator()
+    email_info =  {'recipient': 'Ricardo', 'n_stocks': len(scraped_tb.symbol)}
+    email.send_successful_app_run(email_info)
 
 
 if __name__ == "__main__":
