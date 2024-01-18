91c8d2fd8c069e8dfb20b67056ace6814d434abe
Ricardo Sans <ricardosans98@gmail.com>
2024-01-18 21:12:39 +0100

    Change code in StockExtractor according to new source of information

--- src/data_extractor/stock_extractor.py
+++ src/data_extractor/stock_extractor.py
@@ -1,155 +1,82 @@
-from config.log_config import logger 
 from datetime import datetime, timedelta
-from typing import Dict, Any
+from typing import Union, Tuple
 
 import pandas as pd
-import requests
-from dateutil.relativedelta import relativedelta
+import yfinance as yf
 
+from config.log_config import logger 
 from src.data_extractor.base_extractor import BaseExtractor
-from utils.error_handling import ValueOutOfBoundsException, APIError
 
 
 class StockExtractor(BaseExtractor):
     """Extract stock rates from the AlphaVantage API."""
 
-    ACCEPTABLE_PERIODS = ["1min", "5min", "15min", "30min", "60min", "daily"]
+    VALID_INTERVALS = ["1m", "2m", "5m", "15m", "30m", "60m", "90m", "1h", "1d", "5d", "1wk", "1mo", "3mo"]
 
     def __init__(self, symbol: str):
         super().__init__()
         self.symbol = symbol
         self.url = None
 
-    def get_data(self, 
-                 period: str = "daily", 
-                 from_date: str = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d"), 
-                 until_date: str =(datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")) -> pd.DataFrame:
-        """Get the stock data from the API for a specified symbol.
+    def get_data(self,
+                 end_date: str = datetime.now().strftime('%Y-%m-%d'),
+                 lookback_period: int = 30,
+                 chunk_size: int = 7,
+                 interval: str = "1m")-> pd.DataFrame:
+        """Get OHLCV data for any given stock, with a given frequency, with a maximum lookback period of 30 days.
 
         Args:
-            period (str, optional): Defines the window size for each new quote. Defaults to "daily".
-            from_date (str, optional): Date from where to start fetching data. Only accepts '%Y-m-%d' string formats. 
-                                       Defaults to datetime.now().strftime("%Y-%m-%d") - timedelta(days=1) (yesterday).
-            until_date (str, optional): Date from where to end fetching data. Only accepts '%Y-m-%d' string formats. 
-                                        Defaults to datetime.now().strftime("%Y-%m-%d") - timedelta(days=1) (yesterday).
-
-        Raises:
-            ValueOutOfBoundsException: Raises exception if the 'period' argument is not within the 
-                                       default values from the 'ACCEPTABLE_PERIODS' class attribute.
+            end_date (str): Latest date to compute. Follows the "%Y-%m-%d" format.
+            lookback_period (int): Number of days to look back from the end date. Maximum 30 days.
+            chunk_size (int): Number of days that each chunk must have.
+            interval (str, optional): The interval determines the time span between to consecutive rows in the dataframe.
+                                      Defaults to "1d".
 
         Returns:
-            pd.DataFrame: Returns DataFrame containing OHLCV information.
+            pd.DataFrame: Dataframe with OHLCV data for the given stock.
         """
-        logger.debug(f"Extracting {period} stock information for {self.symbol}.")
-        if period not in self.ACCEPTABLE_PERIODS:
-            raise ValueOutOfBoundsException(
-                f"Argument 'period' must be one of these categories: \
-                                            {', '.join(self.ACCEPTABLE_PERIODS)}"
-            )
-
-        json_rates = {}
-        current_month = datetime.strptime(from_date, "%Y-%m-%d").replace(day=1)
-        last_month = datetime.strptime(until_date, "%Y-%m-%d").replace(day=1)
-        while current_month <= last_month:
-            month_str = current_month.strftime("%Y-%m")
-            new_data = self.__choose_function_type(period, month_str)
-            if new_data:
-                json_rates.update(new_data)
-            logger.debug(
-                f"{self.symbol}: Extracting information for month {month_str}"
-            )
-            current_month += relativedelta(months=1)
-        df = pd.DataFrame(json_rates).T
-        if period == "daily":
-            renamed_cols = {
-                "1. open": "open",
-                "2. high": "high",
-                "3. low": "low",
-                "4. close": "close",
-                "5. adjusted close": "adj_close",
-                "6. volume": "volume",
-                "7. dividend amount": "dividend_amount",
-                "8. split coefficient": "split_coeff",
-            }
-        else:
-            renamed_cols = {
-                "1. open": "open",
-                "2. high": "high",
-                "3. low": "low",
-                "4. close": "close",
-                "5. volume": "volume",
-            }
-        df = df.rename(columns=renamed_cols)
-        df.index = pd.to_datetime(df.index)
-
-        # Apply specific daydate filters
-        start_date = pd.to_datetime(f"{from_date} 00:00:00")
-        end_date = pd.to_datetime(f"{until_date} 23:59:59")
-        df = df[(df.index >= start_date) & (df.index <= end_date)]
-        df = df.apply(pd.to_numeric, errors='ignore')
-        df = df.fillna(method="ffill")
-        df['datetime'] = df.index
-        df['timestamp'] = datetime.utcnow()
-        df['symbol'] = self.symbol
-
+        df_list = []
+        start_dates, end_dates = self.calculate_date_chunks(end_date, lookback_period, chunk_size)
+        for start, end in zip(start_dates, end_dates):
+            aapl = yf.download(tickers=self.symbol, start=start, end=end, interval=interval)
+            df_list.append(aapl)
+        df = pd.concat(df_list)
+        logger.debug(f"Data on {self.symbol} with {len(df)} rows has been imported successfully.")
         return df
 
-    def __choose_function_type(self, period: str, month: str) -> Dict[str, str]:
-        """Decide wich endpoint to trigger depending on the period category.
-
-        Args:
-            period (str): Defines the window size for each new quote. Defaults to "daily".
-            month (str, optional): Timespan of the extracted information. 
-                                   Defaults to datetime.now().strftime("%Y-%m").
-
-        Returns:
-            Dict[str, str]: JSON file containing OHLCV information from the API.
-        """
-        if period == "daily":
-            self.url = (
-                f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol="
-                f"{self.symbol}&outputsize=full&apikey={self.api_key}"
-            )
-            response = self.__return_request(self.url, self.symbol)
-            json_data = response.get("Time Series (Daily)")
-        else:
-            self.url = (
-                f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="
-                f"{self.symbol}&outputsize=full&month={month}&interval={period}&apikey={self.api_key}"
-            )
-            response = self.__return_request(self.url, self.symbol)
-            json_data = response.get(f"Time Series ({period})")
-
-        return json_data
-
     @staticmethod
-    def __return_request(url: str, symbol: str) -> dict:
-        """_summary_
+    def calculate_date_chunks(end_date: str, lookback_period: int, chunk_size: int) -> Union[Tuple[str]]:
+        """Calculate periods of {chunk_size} days according to an end date and a lookback period.
 
         Args:
-            url (str): Endpoint url for the API call.
-
-        Raises:
-            APIError: Raise custom error if any problem arises within the API. 
+            end_date (str): Latest date to compute. Follows the "%Y-%m-%d" format.
+            lookback_period (int): Number of days to look back from the end date. Maximum 30 days.
+            chunk_size (int): Number of days that each chunk must have.
 
         Returns:
-            dict: Returns a dictionary with the stated characteristics.
+            Union[Tuple[str]]: Returns two tuples with start and end days divided into several chunks.
         """
-        try:
-            r = requests.get(url)
-            r_json = r.json()
-            if len(r_json) == 0:
-                logger.warning(f"API response returned an empty dictionary for symbol: {symbol}")
+        date_format = "%Y-%m-%d"
+        end_date = datetime.strptime(end_date, date_format)
+        start_dates = []
+        end_dates = []
 
-            
-            potential_error_message = list(r_json.keys())[0]
-            potential_error_explanation = list(r_json.values())[0]
-            if potential_error_message.lower() == "error message":
-                logger.error(f"{potential_error_explanation}")
-                r_json = {}
+        # Calculate the initial start date for the 30-day lookback period
+        initial_start_date = end_date - timedelta(days = lookback_period - 1)
 
-        except requests.exceptions.RequestException:
-            logger.error(f"Could not connect with AlphaVantage API. Please, "\
-                           "make sure you are connected to the internet")
+        while lookback_period > 0:
+            # Calculate the start date by subtracting the chunk size from the end date
+            start_date = end_date - timedelta(days=chunk_size - 1)  # Adjust chunk_size
+            start_dates.append(start_date.strftime(date_format))
+            end_dates.append(end_date.strftime(date_format))
+            end_date = start_date - timedelta(days=1)  # Set end_date to the day before the start date
+            lookback_period -= chunk_size
 
-        return r_json
+        # Reverse the lists to have them in ascending order
+        start_dates.reverse()
+        end_dates.reverse()
+
+        # Replace the initial start date with the correct start date
+        start_dates[0] = initial_start_date.strftime(date_format)
+
+        return start_dates, end_dates
