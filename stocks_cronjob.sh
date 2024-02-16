133d3261f4578f8a4ea1f1c7d47bc242a423471e
schrutebeet <ricardosans98@gmail.com>
2024-02-16 21:55:21 +0100

    Add stocks_cronjob

---
+++ stocks_cronjob.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Set environment variables
+export LOGS_PATH=/home/schrutebeet/Desktop/logs
+export DATABASE_HOST=localhost
+
+# Change working directory
+cd /home/schrutebeet/Desktop/stock_market
+
+# Set up python environment
+source stock_venv/bin/activate
+
+# Run main file
+python -m src.main
